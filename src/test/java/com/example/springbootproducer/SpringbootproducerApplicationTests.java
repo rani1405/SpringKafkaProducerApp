21f17d8d332b9e24efd2c9fd951192efb49027be
rani1405 <jdpatel029@gmail.com>
2024-09-14 21:11:21 -0400

    Initial commit

---
+++ src/test/java/com/example/springbootproducer/SpringbootproducerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootproducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
